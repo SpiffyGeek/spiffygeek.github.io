54ee63f2371f823c8501abd08bea2c11457b2a6a
SpiffyGeek <52330244+SpiffyGeek@users.noreply.github.com>
2019-06-28 15:09:10 -0500

    finished socket.io tutorial

---
+++ pages/chatrules.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var socket = io();
+    $('form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+    });
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
