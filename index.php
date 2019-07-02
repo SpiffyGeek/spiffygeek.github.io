996cea7a3a1042941e844d8d39ad948b86593c98
SpiffyGeek <52330244+SpiffyGeek@users.noreply.github.com>
2019-07-02 12:24:08 -0500

    established index php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>SpiffyGeek</title>
+</head>
+<body>
+    <center>
+        <h1>Welcome to SpiffyGeek</h1>
+        <p><a href = "/pages/chatroom.html">Chatroom</a></p>
+        <h2>Version: NERDCONTEXT</h2>
+    </center>
+</body>
+</html>
